91c1816f052b7d5c82ff6af303391cb896adc3de
Dell <contactpunitha@yahoo.co.in>
2023-06-20 22:57:00 +0530

    Added few fields like age and address

--- src/main/java/com/mavendemos/mavenprojectdemo/HelloWorld.java
+++ src/main/java/com/mavendemos/mavenprojectdemo/HelloWorld.java
@@ -1,6 +1,9 @@
 package com.mavendemos.mavenprojectdemo;
 
 public class HelloWorld {
+	int age;
+	int address;
+	
 public static void main(String[] args) {
 	System.out.println("First modification");
 	System.out.println("Hello World");
