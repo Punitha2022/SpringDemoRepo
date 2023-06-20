404711999e333c749b45792a2ed8c03483a30c07
Dell <contactpunitha@yahoo.co.in>
2023-06-20 22:56:16 +0530

    Added some changes

--- src/main/java/com/mavendemos/mavenprojectdemo/HelloWorld.java
+++ src/main/java/com/mavendemos/mavenprojectdemo/HelloWorld.java
@@ -2,6 +2,7 @@ package com.mavendemos.mavenprojectdemo;
 
 public class HelloWorld {
 public static void main(String[] args) {
+	System.out.println("First modification");
 	System.out.println("Hello World");
 }
 }
